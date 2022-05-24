e62a8f50972e5b48bfec9e8dd6e9a111a522e077
Mauricio-Silva <sir.silvabmauricio@gmail.com>
2022-05-23 20:18:59 -0400

    Aula_23_05_2022

--- lib/x_scaffold/x_drawer.dart
+++ lib/x_scaffold/x_drawer.dart
@@ -8,64 +8,77 @@ class XDrawer extends StatelessWidget {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: const <Widget>[
+        children: <Widget>[
           // DrawerHeader(),
-          // Stack(
-          //   children: [
-          //     Image.network("https://lastfm.freetls.fastly.net/i/u/ar0/ad05b6bd6e70b95f31b9cb3215a509fc.jpg")
-          //   ],
-          // ),
-          UserAccountsDrawerHeader(
-            accountName: Text("Black Rover"),
-            accountEmail: Text("VickeBlanka - Wizard"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage(
-                "https://cdn.myanimelist.net/images/characters/16/466210.jpg",
-              ),
-            ),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  "https://lastfm.freetls.fastly.net/i/u/ar0/ad05b6bd6e70b95f31b9cb3215a509fc.jpg",
+          Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.transparent,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(
+                      "https://lastfm.freetls.fastly.net/i/u/ar0/ad05b6bd6e70b95f31b9cb3215a509fc.jpg",
+                    ),
+                  ),
                 ),
-                fit: BoxFit.cover,
+                height: 350.0,
               ),
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.yellow,
-                  Colors.black,
-                ],
+              Container(
+                height: 350.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  gradient: LinearGradient(
+                    begin: FractionalOffset.topCenter,
+                    end: FractionalOffset.bottomCenter,
+                    colors: [
+                      Colors.grey.withOpacity(0.0),
+                      Colors.black,
+                    ],
+                    stops: const [0.0, 1.0],
+                  ),
+                ),
               ),
-            ),
+              const UserAccountsDrawerHeader(
+                accountName: Text("Black Rover"),
+                accountEmail: Text("VickeBlanka - Wizard"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    "https://cdn.myanimelist.net/images/characters/16/466210.jpg",
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  color: Color(0x00000000),
+                ),
+              ),
+            ],
           ),
-          ListTile(
+          const ListTile(
             leading: Icon(Icons.library_music),
             title: Text("Biblioteca"),
           ),
-          ListTile(
+          const ListTile(
             leading: Icon(Icons.folder_copy),
             title: Text("Pastas"),
           ),
-          ListTile(
+          const ListTile(
             leading: Icon(Icons.queue_music),
             title: Text("Listas de Reprodução"),
           ),
-          Divider(),
-          ListTile(
+          const Divider(),
+          const ListTile(
             leading: Icon(Icons.timer),
             title: Text("Temporizador"),
           ),
-          ListTile(
+          const ListTile(
             leading: Icon(Icons.equalizer),
             title: Text("Equalizador"),
           ),
-          ListTile(
+          const ListTile(
             leading: Icon(Icons.settings),
             title: Text("Configurações"),
           ),
-          ListTile(
+          const ListTile(
             leading: Icon(Icons.help),
             title: Text("Suporte"),
           ),
